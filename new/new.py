2be5f4bba9023f089ef872d1e3d809f0901cd721
ahmed alfrash <ahmed.alfrash@icloud.com>
2020-11-14 00:36:34 +0200

    added new.py

---
+++ new/new.py
@@ -0,0 +1,8 @@
+# new to vim
+# vim fileName.py to create new file
+# i to go to insert mode
+# escape to exit insert mode and go to normal mode
+# :w to write file in desk to save
+# :q to quit vim
+print("hello world")
+
